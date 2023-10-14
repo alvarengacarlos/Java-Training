e4b14bab393dea89e067ca9b071aefbb8569ecd3
alvarengacarlos <carlosho200@hotmail.com>
2023-10-14 07:34:10 -0400

    Creates simple transfer file project

---
+++ Simple-Transfer-File/src/alvarengacarlos/client/App.java
@@ -0,0 +1,8 @@
+package alvarengacarlos.client;
+
+public class App {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
---
+++ Simple-Transfer-File/src/alvarengacarlos/server/App.java
@@ -0,0 +1,8 @@
+package alvarengacarlos.server;
+
+public class App {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
+}
